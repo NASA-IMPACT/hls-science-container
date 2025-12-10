0af33ab8572ccbbe1eca494aedb170ce673f84a8
Chris Holden <ceholden@gmail.com>
2025-12-10 16:12:53 -0500

    fix: include libXt

--- packages/fmask4/activate.sh
+++ packages/fmask4/activate.sh
@@ -7,7 +7,12 @@ export LD_LIBRARY_PATH_FMASK4=$LD_LIBRARY_PATH
 export FMASK_ROOT=${CONDA_PREFIX}/bin/fmask
 MCR_ROOT=${FMASK_ROOT}/v912
 
+# MCR link paths
 LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${MCR_ROOT}/runtime/glnxa64
 LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${MCR_ROOT}/bin/glnxa64
 LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${MCR_ROOT}/sys/os/glnxa64
+
+# We also need to make sure libXt.so from `xorg-libxt` is linked
+LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${CONDA_PREFIX}/lib
+
 export LD_LIBRARY_PATH
