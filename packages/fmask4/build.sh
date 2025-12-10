5c39bd70759f45eaadc3affd2c811efd09bb2bec
Chris Holden <ceholden@gmail.com>
2025-12-10 17:29:35 -0500

    fix: fmask paths and add to deps

--- packages/fmask4/build.sh
+++ packages/fmask4/build.sh
@@ -11,5 +11,7 @@ chmod +x $INSTALLER_FILE
 cp ${RECIPE_DIR}/run_Fmask.sh ${PREFIX}/bin/
 chmod +x ${PREFIX}/bin/run_Fmask.sh
 
+mkdir -p $PREFIX/etc/conda/activate.d/
 cp $RECIPE_DIR/activate.sh $PREFIX/etc/conda/activate.d/fmask4.sh
+mkdir -p $PREFIX/etc/conda/deactivate.d/
 cp $RECIPE_DIR/deactivate.sh $PREFIX/etc/conda/deactivate.d/fmask4.sh
--- packages/fmask4/run_Fmask.sh
+++ packages/fmask4/run_Fmask.sh
@@ -5,6 +5,6 @@
 # The setting of LD_LIBRARY_PATH has already been done by the "activate.sh"
 # script, so we just need to pass the location of the MCR and auxiliary data.
 
-FMASK_ROOT=${CONDA_PREFIX}/bin/fmask/
+FMASK_ROOT=${CONDA_PREFIX}/bin/fmask4/
 
 ${FMASK_ROOT}/application/Fmask_4_7 ${FMASK_ROOT}/application/AuxiData/
