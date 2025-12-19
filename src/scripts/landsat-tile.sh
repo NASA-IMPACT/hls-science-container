3cf1d25beafc475abd4db50cc1d85a3bd5d9ec97
Chris Holden <ceholden@gmail.com>
2025-12-19 11:40:09 -0500

    feat: import sentinel+landsat shell scripts

---
+++ src/scripts/landsat-tile.sh
@@ -0,0 +1,235 @@
+#!/bin/bash
+
+# Exit on any error
+set -o errexit
+
+jobid="$AWS_BATCH_JOB_ID"
+pathrowlist="$PATHROW_LIST"
+# shellcheck disable=SC2153
+date="$DATE"
+# shellcheck disable=SC2153
+mgrs="$MGRS"
+# shellcheck disable=SC2153
+mgrs_ulx="$MGRS_ULX"
+# shellcheck disable=SC2153
+mgrs_uly="$MGRS_ULY"
+bucket="$OUTPUT_BUCKET"
+inputbucket="$INPUT_BUCKET"
+workingdir="/var/scratch/${jobid}"
+vidir="${workingdir}/vi"
+# shellcheck disable=SC2153
+debug_bucket="$DEBUG_BUCKET"
+gibs_bucket="$GIBS_OUTPUT_BUCKET"
+
+# Remove tmp files on exit
+# shellcheck disable=SC2064
+trap "rm -rf $workingdir; exit" INT TERM EXIT
+
+# Create workingdir
+mkdir -p "$workingdir"
+cd "$workingdir"
+
+year=${date:0:4}
+month=${date:5:2}
+day=${date:8:2}
+day_of_year=$(get_doy "${year}" "${month}" "${day}")
+
+
+set_output_names () {
+  hms="$1"
+  hlsversion="v2.0"
+  outputbasename="T${mgrs}.${year}${day_of_year}T${hms}.${hlsversion}"
+  nbarbasename="${mgrs}.${year}${day_of_year}.${hms}.${hlsversion}"
+  outputname="HLS.L30.${outputbasename}"
+  vi_outputname="HLS-VI.L30.${outputbasename}"
+  # The derive_l8nbar C code infers values from the input file name so this
+  # formatting is necessary.  This implicit name requirement is not documented
+  # anywhere!
+  nbar_name="HLS.L30.${nbarbasename}"
+  nbar_input="${workingdir}/${nbar_name}.hdf"
+  output_hdf="${workingdir}/${outputname}.hdf"
+  nbar_angle="${workingdir}/L8ANGLE.${nbarbasename}.hdf"
+  angleoutputfinal="${workingdir}/${outputname}.ANGLE.hdf"
+  nbar_cfactor="${workingdir}/CFACTOR.${nbarbasename}.hdf"
+  griddedoutput="${workingdir}/gridded.hdf"
+  output_metadata="${workingdir}/${outputname}.cmr.xml"
+  output_stac_metadata="${workingdir}/${outputname}_stac.json"
+  output_thumbnail="${workingdir}/${outputname}.jpg"
+  manifest_name="${outputname}.json"
+  manifest="${workingdir}/${manifest_name}"
+  gibs_dir="${workingdir}/gibs"
+  gibs_bucket_key="s3://${gibs_bucket}/L30/data/${year}${day_of_year}"
+  bucket_key="s3://${bucket}/L30/data/${year}${day_of_year}/${outputname}"
+  vi_bucket_key="s3://${bucket}/L30_VI/data/${year}${day_of_year}/${vi_outputname}"
+}
+
+# Create array from pathrowlist
+IFS=','
+read -r -a pathrows <<< "$pathrowlist"
+
+# Download files
+echo "Download date pathrow intermediate ac files"
+INDEX=0
+for pathrow in "${pathrows[@]}"; do
+  basename="${date}_${pathrow}"
+  landsat_ac="${basename}.hdf"
+  input_bucket_key="${inputbucket}/${year}-${month}-${day}/${pathrow}"
+  landsat_sz_angle="${basename}_SZA.img"
+  aws s3 cp "s3://${input_bucket_key}" "$workingdir" --recursive
+  # Use the scene_time of the first image for output naming
+  if [ "$INDEX" = 0 ]; then
+    scene_time=$(extract_landsat_hms.py "$landsat_ac")
+    set_output_names "$scene_time"
+  fi
+  # shellcheck disable=SC2219
+  let INDEX=${INDEX}+1
+  # Pre-create output file to avoid L8inS2tile failure when removing.
+  echo "Running L8inS2tile ${pathrow}"
+  L8inS2tile  "$mgrs" \
+              "$mgrs_ulx" \
+              "$mgrs_uly" \
+              "NONE" \
+              "NONE" \
+              "$landsat_ac" \
+              "$nbar_input"
+
+  echo "Running angle_tile ${pathrow}"
+  angle_tile  "$mgrs" \
+              "$mgrs_ulx" \
+              "$mgrs_uly" \
+              "$landsat_sz_angle" \
+              "$nbar_angle"
+done
+
+ls "$workingdir"
+if [[ -f "$nbar_input" ]] && [[ -f "$nbar_angle" ]] ; then
+  echo "Running NBAR"
+  # Copy intermediate gridded output for debugging analysis.
+  cp "$nbar_input" "$griddedoutput"
+  derive_l8nbar "$nbar_input" "$nbar_angle" "$nbar_cfactor"
+
+  # Rename nbar to correct output name
+  echo "Rename NBAR"
+  mv "$nbar_input" "$output_hdf"
+  mv "${nbar_input}.hdr" "${output_hdf}.hdr"
+
+  # Rename angle to correct output name
+  mv "$nbar_angle" "$angleoutputfinal"
+
+  # Convert to COGs
+  echo "Converting to COGs"
+  hdf_to_cog "$output_hdf" --output-dir "$workingdir" --product L30
+  hdf_to_cog "$angleoutputfinal" --output-dir "$workingdir" --product L30_ANGLES
+
+  # Create thumbnail
+  echo "Creating thumbnail"
+  create_thumbnail -i "$workingdir" -o "$output_thumbnail" -s L30
+
+  # Create metadata
+  echo "Creating metadata"
+  create_metadata "$output_hdf" --save "$output_metadata"
+
+  # Create STAC metadata
+  echo "Creating STAC metadata"
+  cmr_to_stac_item "$output_metadata" "$output_stac_metadata" \
+    data.lpdaac.earthdatacloud.nasa.gov 020
+
+  # Generate manifest
+  echo "Generating manifest"
+  create_manifest "$workingdir" "$manifest" "$bucket_key" "HLSL30" \
+    "$outputname" "$jobid" false
+
+  # Copy output to S3.
+  mkdir -p ~/.aws
+  echo "[profile gccprofile]" > ~/.aws/config
+  echo "region=us-east-1" >> ~/.aws/config
+  echo "output=text" >> ~/.aws/config
+
+  echo "[gccprofile]" > ~/.aws/credentials
+  echo "role_arn = ${GCC_ROLE_ARN}" >> ~/.aws/credentials
+  echo "credential_source = Ec2InstanceMetadata" >> ~/.aws/credentials
+
+  if [ -z "$debug_bucket" ]; then
+    aws s3 cp "$workingdir" "$bucket_key" --exclude "*" --include "*.tif" \
+      --include "*.xml" --include "*.jpg" --include "*_stac.json" \
+      --profile gccprofile --recursive --quiet
+
+    # Copy manifest to S3 to signal completion.
+    aws s3 cp "$manifest" "${bucket_key}/${manifest_name}" --profile gccprofile
+  else
+    echo "Create gridded debug COG"
+    hdf_to_cog "$griddedoutput" --output-dir "$workingdir" --product L30 --debug-mode
+    # Copy all intermediate files to debug bucket.
+    echo "Copy files to debug bucket"
+    debug_bucket_key=s3://${debug_bucket}/${outputname}
+    aws s3 cp "$workingdir" "$debug_bucket_key" --recursive --quiet --acl public-read
+  fi
+
+  # Generate GIBS browse subtiles
+  echo "Generating GIBS browse subtiles"
+  mkdir -p "$gibs_dir"
+  granule_to_gibs "$workingdir" "$gibs_dir" "$outputname"
+  for gibs_id_dir in "$gibs_dir"/* ; do
+      if [ -d "$gibs_id_dir" ]; then
+        gibsid=$(basename "$gibs_id_dir")
+        echo "Processing gibs id ${gibsid}"
+        # shellcheck disable=SC2206
+        xmlfiles=(${gibs_id_dir}/*.xml)
+        xml="${xmlfiles[0]}"
+        subtile_basename=$(basename "$xml" .xml)
+        subtile_manifest_name="${subtile_basename}.json"
+        subtile_manifest="${gibs_id_dir}/${subtile_manifest_name}"
+        gibs_id_bucket_key="$gibs_bucket_key/${gibsid}"
+        echo "Gibs id bucket key is ${gibs_id_bucket_key}"
+
+        create_manifest "$gibs_id_dir" "$subtile_manifest" \
+          "$gibs_id_bucket_key" "HLSL30" "$subtile_basename" "$jobid" true
+
+        # Copy GIBS tile package to S3.
+        if [ -z "$debug_bucket" ]; then
+          aws s3 cp "$gibs_id_dir" "$gibs_id_bucket_key" --exclude "*"  \
+            --include "*.tif" --include "*.xml" --profile gccprofile \
+            --recursive --quiet
+
+          # Copy manifest to S3 to signal completion.
+          aws s3 cp "$subtile_manifest" \
+            "${gibs_id_bucket_key}/${subtile_manifest_name}" \
+            --profile gccprofile
+        else
+          # Copy all intermediate files to debug bucket.
+          debug_bucket_key=s3://${debug_bucket}/${outputname}
+          aws s3 cp "$gibs_id_dir" "$debug_bucket_key" --recursive --quiet --acl public-read
+        fi
+      fi
+  done
+  echo "All GIBS tiles created"
+else
+  echo "No output tile produced"
+  exit 5
+fi
+
+# Generate VI files
+echo "Generating VI files"
+vi_generate_indices -i "$workingdir" -o "$vidir" -s "$outputname"
+vi_generate_metadata -i "$workingdir" -o "$vidir"
+vi_generate_stac_items --cmr_xml "$vidir/${vi_outputname}.cmr.xml" --endpoint data.lpdaac.earthdatacloud.nasa.gov --version 020 --out_json "$vidir/${vi_outputname}_stac.json"
+
+echo "Generating VI manifest"
+vi_manifest_name="${vi_outputname}.json"
+vi_manifest="${vidir}/${vi_manifest_name}"
+create_manifest "$vidir" "$vi_manifest" "$vi_bucket_key" "HLSL30_VI" \
+  "$vi_outputname" "$jobid" false
+
+if [ -z "$debug_bucket" ]; then
+  aws s3 cp "$vidir" "$vi_bucket_key" --exclude "*" --include "*.tif" \
+    --include "*.xml" --include "*.jpg" --include "*_stac.json" \
+    --profile gccprofile --recursive
+
+  # Copy manifest to S3 to signal completion.
+  aws s3 cp "$vi_manifest" "${vi_bucket_key}/${vi_manifest_name}" --profile gccprofile
+else
+  # Copy all vi files to debug bucket.
+  echo "Copy files to debug bucket"
+  debug_bucket_key=s3://${debug_bucket}/${outputname}
+  aws s3 cp "$vidir" "$debug_bucket_key" --recursive --acl public-read
+fi
---
+++ src/scripts/landsat.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+
+# Exit on any error
+set -o errexit
+
+jobid="$AWS_BATCH_JOB_ID"
+# shellcheck disable=2153
+granule="$GRANULE"
+bucket="$OUTPUT_BUCKET"
+inputbucket="$INPUT_BUCKET"
+# shellcheck disable=2153
+prefix="$PREFIX"
+workingdir="/var/scratch/${jobid}"
+granuledir="${workingdir}/${granule}"
+
+# Remove tmp files on exit
+# shellcheck disable=2064
+trap "rm -rf $workingdir; exit" INT TERM EXIT
+
+rename_angle_bands () {
+  anglebasename=$1
+  newbasename=$2
+  mv "${anglebasename}_VAA.hdr" "${newbasename}_VAA.hdr"
+  mv "${anglebasename}_VAA.img" "${newbasename}_VAA.img"
+  mv "${anglebasename}_VZA.hdr" "${newbasename}_VZA.hdr"
+  mv "${anglebasename}_VZA.img" "${newbasename}_VZA.img"
+  mv "${anglebasename}_SAA.hdr" "${newbasename}_SAA.hdr"
+  mv "${anglebasename}_SAA.img" "${newbasename}_SAA.img"
+  mv "${anglebasename}_SZA.hdr" "${newbasename}_SZA.hdr"
+  mv "${anglebasename}_SZA.img" "${newbasename}_SZA.img"
+}
+
+# Create workingdir
+mkdir -p "$granuledir"
+
+
+echo "Start processing granules"
+
+echo "Copying granule from USGS S3"
+granule=$(download_landsat "$inputbucket" "$prefix" "$granuledir")
+
+fmask="${granule}_Fmask4.tif"
+fmaskbin=fmask.bin
+
+IFS='_'
+read -ra granulecomponents <<< "$granule"
+date=${granulecomponents[3]:0:8}
+year=${date:0:4}
+month=${date:4:2}
+day=${date:6:2}
+pathrow=${granulecomponents[2]}
+outputname="${year}-${month}-${day}_${pathrow}"
+bucket_key="${bucket}/${year}-${month}-${day}/${pathrow}"
+
+
+# Check solar zenith angle.
+echo "Check solar azimuth"
+mtl="${granuledir}/${granule}_MTL.txt"
+solar_zenith_valid=$(check_solar_zenith_landsat "$mtl")
+if [ "$solar_zenith_valid" == "invalid" ]; then
+  echo "Invalid solar zenith angle. Exiting now"
+  exit 3
+fi
+
+# Enter working directory
+cd "$granuledir"
+
+# ovr and IMD files in AWS PDS break Fmask
+# rm *.ovr
+# rm *.IMD
+
+# Run Fmask
+run_Fmask.sh >> fmask_out.txt
+
+# Convert to flat binary
+gdal_translate -of ENVI "$fmask" "$fmaskbin"
+
+# Convert data from tiled to scanline for espa formatting
+echo "Convert to scanline"
+for f in *.TIF
+  do
+  gdal_translate -co TILED=NO "$f" "${f}_scan.tif"
+  rm "$f"
+  mv "${f}_scan.tif" "$f"
+  done
+
+espa_xml="${granule}.xml"
+hls_espa_xml="${granule}_hls.xml"
+srhdf="sr.hdf"
+outputhdf="${outputname}.hdf"
+
+# Convert to espa format
+echo "Convert to ESPA"
+convert_lpgs_to_espa --mtl="$mtl"
+
+# Run lasrc
+echo "Run lasrc"
+do_lasrc_landsat.py --xml "$espa_xml"
+
+# Rename Angle bands to align with Collection 2 naming.
+echo "Rename angle bands"
+rename_angle_bands "${granule}" "$outputname"
+
+# Create ESPA xml file using HLS v1.5 band names
+echo "Create updated espa xml"
+create_landsat_sr_hdf_xml "$espa_xml" "$hls_espa_xml"
+
+# Convert ESPA xml file to HDF
+echo "Convert to HDF"
+convert_espa_to_hdf --xml="$hls_espa_xml" --hdf="$srhdf"
+
+# Run addFmaskSDS
+echo "Run addFmaskSDS"
+aerosol_qa="${granule}_sr_aerosol_qa.img"
+addFmaskSDS "$srhdf" "$fmaskbin" "$aerosol_qa" "$mtl" "$ACCODE" "$outputhdf"
+
+if [[ -z "$DEBUG_BUCKET" ]]; then
+  aws s3 cp "${outputhdf}" "s3://${bucket_key}/${outputname}.hdf"
+  aws s3 cp "$granuledir" "s3://${bucket_key}" --exclude "*" --include "*_VAA.img" \
+    --include "*_VAA.hdr" --include "*_VZA.hdr" --include "*_VZA.img" \
+    --include "*_SAA.hdr" --include "*_SAA.img" --include "*_SZA.hdr" \
+    --include "*_SZA.img" --recursive --quiet
+else
+  debug_bucket="$DEBUG_BUCKET"
+  # Copy all intermediate files to debug bucket.
+  echo "Copy files to debug bucket"
+  timestamp=$(date +'%Y_%m_%d_%H_%M')
+  debug_bucket_key=s3://${debug_bucket}/${granule}_${timestamp}
+  aws s3 cp "$granuledir" "$debug_bucket_key" --recursive --quiet
+fi
