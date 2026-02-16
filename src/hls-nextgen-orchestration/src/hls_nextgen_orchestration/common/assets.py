38eba155b3adf55124ca6b8daf25fcc15a927557
Chris Holden <ceholden@gmail.com>
2026-02-16 17:28:59 -0500

    feat: improve type hinting

--- src/hls-nextgen-orchestration/src/hls_nextgen_orchestration/common/assets.py
+++ src/hls-nextgen-orchestration/src/hls_nextgen_orchestration/common/assets.py
@@ -1,6 +1,14 @@
 from dataclasses import dataclass
+from pathlib import Path
 
 
+# ----- Asset type definitions
+# We can't use `isinstance` on generics like `list[str]`, but we can
+# define a subclass to allow type hints AND runtime checks.
+class Paths(list[Path]): ...
+
+
+# ----- Data containers
 @dataclass(frozen=True)
 class ProcessingMetadata:
     """
